c5105483a4b62b53b1564519ca9635da1d943888
FrancoisTM <francoisthibdemais@outlook.com>
2019-09-26 15:09:50 +0200

    blop null code

--- script.js
+++ script.js
@@ -3,16 +3,25 @@ let difficulte = document.getElementById("mode");
 let messageAccueil = document.getElementById("accueil");
 let messageGagne = document.getElementById("win");
 let messagePerdu = document.getElementById("lose");
-let valeurBouton = document.getElementsByTagName("button")
+let valeurBouton = document.getElementsByTagName("button");
+
 
 //proposer un mot au hasard//
 let rand = motaDeviner[Math.floor(Math.random()*motaDeviner.length)];
     console.log(rand);
-    console.log(rand.length);
-    console.log(rand[0]);
 
 //afficher le mot à deviner en remplaçant les lettres par des underscores//
     let mot = rand.replace(/[a-z]/gi, ' _ ');
     console.log(mot);
     document.getElementById("word").innerHTML = mot;
 //
+let button = document.getElementsByTagName("button");
+console.log(button);
+
+
+    button.addEventListener("click", () => {
+
+        let valeur = button.getAttribute("value")
+        console.log(valeur);
+    });
+
