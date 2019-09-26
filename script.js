c42c6456bcc808c72f421c64b146867f0bc4c841
MatthieuDuranton <matthieu.duranton@hotmail.be>
2019-09-26 12:04:07 +0200

    essai display

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-let motaDeviner = document.getElementById("guess");
-let difficulte = document.getElementById("mode").value;
+let motaDeviner = ['coucou', 'anticonstitutionnellement', 'monde', 'carrelage'];
+let difficulte = document.getElementById("mode");
 let messageAccueil = document.getElementById("accueil");
 let messageGagne = document.getElementById("win");
 let messagePerdu = document.getElementById("lose");
 let valeurBouton = document.getElementsByTagName("button")
+
+//proposer un mot au hasard//
+let rand = motaDeviner[Math.floor(Math.random()*motaDeviner.length)];
+    console.log(rand);
+    console.log(rand.length);
+    console.log(rand[0]);
+
+//afficher le mot à deviner en remplaçant les lettres par des underscores//
+    let mot = rand.replace(/[a-z]/gi, ' _ ');
+    console.log(mot);
+    document.getElementsByTagName("h1").innerhtml = mot
+//
