0b28f4fe8d5a276449834546c49b3ba8711b461a
MatthieuDuranton <matthieu.duranton@hotmail.be>
2019-09-26 11:16:46 +0200

    ids

---
+++ script.js
@@ -0,0 +1,6 @@
+let motaDeviner = document.getElementById("guess");
+let difficulte = document.getElementById("mode").value;
+let messageAccueil = document.getElementById("accueil");
+let messageGagne = document.getElementById("win");
+let messagePerdu = document.getElementById("lose");
+let valeurBouton = document.getElementsByTagName("button")
