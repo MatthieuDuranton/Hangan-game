a5462ec62aea8994678cec2751f60fdd95489dd9
MatthieuDuranton <matthieu.duranton@hotmail.be>
2019-09-30 09:27:44 +0200

    photos

--- script.js
+++ script.js
@@ -6,24 +6,19 @@ var lettre = "a"
 //proposer un mot au hasard//
 var motMystere = motaDeviner[Math.floor(Math.random()*motaDeviner.length)];
     console.log(motMystere);
-    console.log(motMystere[0]);
 
 //afficher le mot à deviner en remplaçant les lettres par des questionMarks//
 var questionMarks = motMystere.replace(/[a-z]/gi, '?');
-    console.log(questionMarks);
     
 document.getElementById("word").innerHTML = questionMarks;//afficher les questionMarks sur page avec variable word
 
 //récupérer la lettre du bouton appuyé et actions à effectuer//    
 
 function GFG_click(letter) {//récupérer l'ID du bouton 
-    console.log(letter);
     lettre = String(letter);//transformer l'ID en string
-    console.log(lettre);
     for (i=0; i<motMystere.length; i++){//remplacer le questionMarks si on a une lettre qui correspond
         if (lettre === motMystere[i]){
             questionMarks = questionMarks.substring(0, i) + lettre + questionMarks.substring(i+1);//je dois casser mon string en 2. Je ne peux pas le modifier directement en JS
-            console.log(questionMarks);
         }
     }
     document.getElementById("word").innerHTML = questionMarks
